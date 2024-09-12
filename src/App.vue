3305e01a0a16d7023466c9e0487695e27eb645dd
kimatron <kimmykim@gmail.com>
2024-09-12 20:29:52 +0100

    Add navigation links :sparkle:

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/map">Dive Site Map</router-link>
+      <router-link to="/map">Dive Site Map</router-link> |
+      <router-link to="/list">Dive Site List</router-link>
     </nav>
     <router-view></router-view>
   </div>
@@ -21,6 +22,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>

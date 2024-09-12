bdf7429bba38157ffd63493bc22621806750768d
kimatron <kimmykim@gmail.com>
2024-09-12 20:06:55 +0100

    Add home component :sparkle:

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h2>Welcome to Dive Site Explorer</h2>
+    <p>Discover amazing dive sites around the world!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home-Page'
+}
+</script>

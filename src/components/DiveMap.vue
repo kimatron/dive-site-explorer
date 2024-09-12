0ebcb1659ca38a1a5bdedd03ecf13400f1543175
kimatron <kimmykim@gmail.com>
2024-09-12 20:08:10 +0100

    Add Dive Map component :sparkle:

---
+++ src/components/DiveMap.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dive-site-map">
+    <h2>Dive Site Map</h2>
+    <div id="mapContainer" style="height: 400px;"></div>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default {
+  name: 'DiveSiteMap',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data() {
+    return {
+      center: [0, 0],
+      zoom: 2,
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }
+  }
+}
+</script>
